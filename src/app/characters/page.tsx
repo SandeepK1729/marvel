fca5ae84f67d4e718176c3fa85dc621f105b14d0
Konda Sandeep <139792713+SandeepOCT@users.noreply.github.com>
2024-11-24 19:48:31 +0530

    fix bug with build

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -2,12 +2,13 @@ import Character from "@/components/character";
 import { fetch } from "@/services";
 import { CharactersResponse } from "@/types";
 
+
 export default async function Page() {
   const charactersResponse = await fetch<CharactersResponse>("/v1/public/characters");
   const characters = charactersResponse.data.results;
 
   return (
-    <div>
+    <div style={{ display: 'flex', gap: '1%' }}>
       {characters.map((character) => (
         <Character key={character.id} character={character} />
       ))}
--- src/components/character.tsx
+++ src/components/character.tsx
@@ -1,17 +1,44 @@
+"use client";
 import type { Character } from "@/types";
 import Image from "next/image";
+import Link from "next/link";
+
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
 
 export default function Character({ character }: { character: Character }) {
+  /*
+  - comics
+  - series
+  - stories
+  - events
+  - urls
+  */
   return (
-    <div>
-      <h2>{character.name}</h2>
-      <p>{character.description}</p>
-      <Image
-        src={character.thumbnail.path + "." + character.thumbnail.extension}
-        alt={character.name}
-        width={200}
-        height={200}
-      />
+    <div style={{ width: '20%' }}>
+      <Link href={`/characters/${character.id}`}>
+        <Card sx={{  }}>
+          <CardMedia />
+            <Image
+              src={character.thumbnail.path + "." + character.thumbnail.extension}
+              alt={character.name}
+              width={345}
+              height={345}
+            />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {character.name}
+            </Typography>
+            {character.description && <Typography variant="body2" color="text.secondary">
+              <q>{character.description}</q>
+            </Typography>}
+          </CardContent>
+        </Card>
+      </Link>
     </div>
   );
 }
